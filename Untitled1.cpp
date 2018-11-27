29d616fa1e6f15638f7f9d5c7498337261e462e6
NachoGranados <ignaciogramar@gmail.com>
2018-11-27 00:21:38 -0600

    Se agregan las funciones de eliminar y dar_informacion

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -147,6 +147,187 @@ void agregar_usuario(string identificacion){
 	
 }
 
+void eliminar(string identificacion,string opcion){
+	
+	// Definicion de variables a utilizar.
+	ifstream archivo;
+	ifstream otro;
+	ofstream nuevo;
+	string guia;
+	string linea;
+	string datos;
+	string nombre;
+	bool membresia = false;
+	
+	// Creacion y edicion de archvo.
+	nombre = identificacion + "(AD).txt";
+	otro.open(nombre.c_str(),ios::in);
+	
+	getline(otro,guia);
+	
+	otro.close();
+	
+	archivo.open("Usuarios.txt",ios::in);
+	
+	while(!archivo.eof()){ // Iteracion que finaliza cuando se llega a la ultima linea del mismo.
+		
+		getline(archivo,linea);
+	
+		if(linea == identificacion){ // Cindicion que verifica se identificacion existe.
+			
+			membresia = true;
+			
+		}
+	
+	}
+	
+	archivo.close();
+	
+	if(membresia == true){
+		
+		if(guia == "desarmado"){
+			
+			archivo.open("Usuarios.txt",ios::in);
+		
+			while(!archivo.eof()){ // Iteracion que finaliza cuando se llega a la ultima linea del mismo.
+			
+				getline(archivo,linea);
+				
+				if(linea != identificacion){ // Condicion que si la identificacion es diferente a la linea del archivo
+					
+					datos = datos + linea + "\n"; // Se agrega la linea a un string.
+						
+				}else{
+					
+					for (int i = 0; i < 6; i++){ // Iteracion para eliminar lineas
+						
+						getline(archivo,linea);
+					
+					}
+					
+				}
+					
+			}
+			
+			// Creacion y edicion de archvo.
+			nuevo.open("Usuarios.txt",ios::out);
+			nuevo << datos;
+			nuevo.close();
+			
+			// Eliminacion del archvo.
+			nombre = identificacion + "(estado).txt";
+			remove(nombre.c_str());
+			
+			// Eliminacion del archvo.
+			nombre = identificacion + "(AD).txt";
+			remove(nombre.c_str());
+			
+			// Eliminacion del archvo.
+			nombre = identificacion + "(Zonas).txt";
+			remove(nombre.c_str());
+			
+			// Eliminacion del archvo.
+			nombre = identificacion + "(Bitacora).txt";
+			remove(nombre.c_str());
+			
+			// Eliminacion del archvo.
+			nombre = identificacion + "(Principal).txt";
+			remove(nombre.c_str());
+			
+			// Eliminacion del archvo.
+			nombre = identificacion + "(Secundarias).txt";
+			remove(nombre.c_str());
+
+			if(opcion == "Si" or opcion == "si"){
+				
+				agregar_usuario(identificacion);
+				
+			}
+				
+		}else{
+			
+			cout << "No se puede eliminar el usuario porque el sistema se encuentra armado \n \n"; // Impresion en pantalla.
+			
+		}
+		
+	}else{
+		
+		cout << "No se puede eliminar porque el usuario no exite \n \n"; // Impresion en pantalla.
+		
+	}
+		
+}
+
+void dar_informacion(string identificacion){
+	
+	// Definicion de variables a utilizar.
+	ifstream archivo;
+	string linea;
+	string opcion;
+	
+	archivo.open("Usuarios.txt",ios::in);
+	
+	while(!archivo.eof()){ // Iteracion que finaliza cuando se llega a la ultima linea del mismo.
+	
+		getline(archivo,linea);
+		
+		if(linea == identificacion){
+			
+			 // Impresion en pantalla.
+			cout << "Identificacion del Sistema: " << linea << "\n";
+			
+			getline(archivo,linea);
+			
+			 // Impresion en pantalla.
+			cout << "Usuario: " << linea << "\n";
+			
+			getline(archivo,linea);
+			
+			 // Impresion en pantalla.
+			cout << "Propiedad: " << linea << "\n";
+			
+			getline(archivo,linea);
+			
+			 // Impresion en pantalla.
+			cout << "Direccion fisica: " << linea << "\n";
+			
+			getline(archivo,linea);
+			
+			 // Impresion en pantalla.
+			cout << "Telefono 1: " << linea << "\n";
+			
+			getline(archivo,linea);
+			
+			 // Impresion en pantalla.
+			cout << "Telefono 2: " << linea << "\n";
+			
+			getline(archivo,linea);
+			
+			 // Impresion en pantalla.
+			cout << "Correo electronico: " << linea << "\n \n";
+			
+		}
+	
+	}
+	
+	archivo.close();
+
+	 // Impresion en pantalla.
+	cout << "Desea cambiar la informacion actual? (Si o No) \n>>> ";
+	cin >> opcion;
+	cout << "\n";
+	
+	system("cls");
+	
+	if(opcion == "Si" or opcion == "si"){
+	
+		eliminar(identificacion,opcion);
+		
+	}
+	
+	system("cls");
+	
+}
 
 
 
